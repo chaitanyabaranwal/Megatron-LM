4921a2a0615fb0cb5308dd45514e92fd91c0deda
Chaitanya Baranwal <chaitanyabaranwal@gmail.com>
2022-03-19 02:15:05 +0800

    Add min config

---
+++ scripts/bert_distributed_min.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+module load daint-gpu
+module load PyTorch
+
+np=${1:-"1"}
+pipeline=${2:-"1"}
+tensor=${3:-"4"}
+seq=${4:-"128"}
+mic_bs=${5:-"8"}
+glb_bs=${6:-"8"}
+layer=${7:-"6"}
+hidden=${8:-"256"}
+heads=${9:-"8"}
+iters=${10:-"210"}
+
+python ./scripts/get_host_ip_addr.py > "./HOST"
+ADDR=`cat ./HOST`
+export MASTER_ADDR=$ADDR
+export MASTER_PORT=29500
+export PIPELINE_PARALLEL_SIZE=$pipeline
+export TENSOR_PARALLEL_SIZE=$tensor
+export NNODES=$np
+export SEQ_LENGTH=$seq
+export MICRO_BATCH_SIZE=$mic_bs
+export GLOBAL_BATCH_SIZE=$glb_bs
+export NUM_LAYERS=$layer
+export HIDDEN_SIZE=$hidden
+export NUM_HEADS=$heads
+export TRAIN_ITERS=$iters
+
+srun bash ./examples/pretrain_bert_distributed.sh
